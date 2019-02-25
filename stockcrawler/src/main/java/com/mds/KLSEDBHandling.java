9da771cb28aee7aec855d933717f4be9ac2b23e3
johnlplaw <34481296+johnlplaw@users.noreply.github.com>
2019-02-25 23:06:07 +0800

    Update KLSEDBHandling.java
    Add function for insert Company Category

--- stockcrawler/src/main/java/com/mds/KLSEDBHandling.java
+++ stockcrawler/src/main/java/com/mds/KLSEDBHandling.java
@@ -4,15 +4,21 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.util.ArrayList;
-
+/**
+ * The class file for handling the mysql database transaction
+ *
+ */
 public class KLSEDBHandling {
 
 	public static void main(String[] args) {
 		KLSEDBHandling klse = new KLSEDBHandling();
 		
-
 	}
 	
+	/**
+	 * Insert the klsedata into KLSE table in mysql
+	 * @param theList
+	 */
 	public void insertKLSEData(ArrayList<KLSEData> theList) {
 		
 		Connection conn = null;
@@ -57,6 +63,59 @@ public class KLSEDBHandling {
                 
             	stmt.addBatch();
                 
+            }
+            
+            stmt.executeBatch();
+            
+        } catch (Exception e) {
+        	e.printStackTrace();
+        } finally {
+            try {
+				if (stmt != null)
+					stmt.close();
+			} catch (Exception e2) {
+				
+			}
+			try {
+				if (conn != null)
+					conn.close();
+			} catch (Exception e2) {
+			}
+            
+            
+        }
+	}
+
+	/**
+	 * Insert the Company Category into  table in CompCat
+	 * @param theList
+	 */
+	public void insertCompCat(ArrayList<String> theList, String CompCat) {
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		
+		try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager
+                    .getConnection("jdbc:mysql://localhost/mdsklse?"
+                            + "user=root&password=root");
+
+            String sqlStr = "INSERT INTO CompCat ( " + 
+            		"compCode, " + 
+            		"Category, " + 
+            		"Sub_category, " + 
+            		"crawl_Timestamp) values ("
+            		+ "?, ?, ?, SYSDATE() )";
+            
+            stmt = conn.prepareStatement(sqlStr);
+            
+            for(String compCode : theList) {
+            	String[] catCodeArray = CompCat.split("_");
+            	stmt.setString(1, compCode);
+            	stmt.setString(2, catCodeArray[0]);
+            	stmt.setString(3, catCodeArray[1]);
+                
+            	stmt.addBatch();             
             }
             
             stmt.executeBatch();
